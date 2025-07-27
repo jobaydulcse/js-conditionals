8ba04470bf3b6ce2bdc7b509bd081a221857b5ab
jobaydulcse <jobaydul.cse@gmail.com>
2025-07-28 02:34:30 +0600

    if else relataded work

--- comparison.js
+++ comparison.js
@@ -1,3 +1,25 @@
-console.log(5>10);
-console.log(5<10);
-console.log( 5==10 );
+console.log(5 > 10);
+console.log(5 < 10);
+console.log(5 == 10);
+console.log(10 == 10);
+console.log(10 >= 10);
+console.log(15 >= 10);
+
+// -compare
+var applePrice = 350;
+var peyaraPrice = 80;
+
+console.log(applePrice < peyaraPrice);
+console.log(applePrice > peyaraPrice);
+
+// ==
+// ===
+
+console.log(10 == "10");
+console.log(10 === "10");
+
+
+// --------------------------------
+console.log( 10 != '10');
+console.log( 10 !== '10');
+
---
+++ if.js
@@ -0,0 +1,25 @@
+// -------------------------------------------------
+// if condition
+if(3<10)
+{console.log('I Have A Small Value')
+
+}
+// ------------------------------------------------
+var biryaniPrice = 250;
+if(biryaniPrice < 300)
+    {
+console.log('Give me  some biriyni')
+}
+else{
+    console.log('Biriyani diyen nah');
+}
+// ---------------------------------------------------
+var weight = 10;
+if (weight<20){
+    console.log('I will carry it by myself');
+}
+else{
+    console.log('I will rent a rickshow');
+}
+
+
