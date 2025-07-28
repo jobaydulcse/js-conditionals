3dcbb40e5d4e0aae90bbdc144a41cf6dac9b5f67
jobaydulcse <jobaydul.cse@gmail.com>
2025-07-28 14:12:13 +0600

    gpa,multiple,resturent,stepped file added

---
+++ gpa.js
@@ -0,0 +1,17 @@
+const result = 20;
+
+if (result < 33) {
+  console.log("Grade: F");
+} else if (result <= 39) {
+  console.log("Grade: D");
+} else if (result <= 49) {
+  console.log("Grade: C");
+} else if (result <= 59) {
+  console.log("Grade: B");
+} else if (result <= 69) {
+  console.log("Grade: A-");
+} else if (result <= 79) {
+  console.log("Grade: A");
+} else {
+  console.log("Grade: A+");
+}
